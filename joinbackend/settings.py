fffe632c315db35b6b63a1ff0324fdb631b9239b
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-19 13:09:22 +0100

    changed settings.py

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'joinbackend.urls'
 
 TEMPLATES = [
@@ -130,9 +132,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join('/home/Alex85', 'join-frontend'),
-]
 
 
 # Default primary key field type
