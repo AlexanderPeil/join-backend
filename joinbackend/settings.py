4454e1cef7786071fc968c6c74cf70fc93681693
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-19 13:20:13 +0100

    added DIRS setup

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -69,7 +69,9 @@ ROOT_URLCONF = 'joinbackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, '../join-frontend')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
